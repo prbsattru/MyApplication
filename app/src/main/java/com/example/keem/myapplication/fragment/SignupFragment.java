7040dc0c1be8acfaf33390ef47ea94620b5f5da8
Praew <teeranan39@hotmail.com>
2017-08-06 12:18:51 +0700

    6/08

---
+++ app/src/main/java/com/example/keem/myapplication/fragment/SignupFragment.java
@@ -0,0 +1,38 @@
+package com.example.keem.myapplication.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.keem.myapplication.R;
+
+
+/**
+ * Created by masterung on 8/6/2017 AD.
+ */
+
+public class SignUpFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_signup, container, false);
+        return view;
+
+    }   // onCreateView
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //Back Controller
+        ImageView imageView = getView().findViewById(R.id.imvBack);
+
+
+    }
+}   // Main Class
